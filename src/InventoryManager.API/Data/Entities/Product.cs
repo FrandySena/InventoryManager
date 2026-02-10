be4cfe61a81eecc2b58d20e1882d26ccf53c0f9c
Frandy Sena <frandysena2@gmail.com>
2026-02-10 15:53:41 -0400

    Adding EF to the project

---
+++ src/InventoryManager.API/Data/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+
+namespace InventoryManager.API.Data.Entities
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(100)]
+        public string Name { get; set; }
+        [StringLength(200)]
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
---
+++ src/InventoryManager.API/Data/InventoryManagerContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManager.API.Data
+{
+    public class InventoryManagerContext : DbContext
+    {
+        public InventoryManagerContext(DbContextOptions<InventoryManagerContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Entities.Category> Categories { get; set; }
+        public DbSet<Entities.Product> Products { get; set; }
+    }
+}
